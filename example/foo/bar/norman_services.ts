76727fc65c0fe9c22b5c20248c425c646b95ee43
Mason Kramer <mason.kramer@gmail.com>
2017-03-12 22:16:25 -0700

    normanc & proper modulization of norman npm

---
+++ example/foo/bar/norman_services.ts
@@ -0,0 +1,23 @@
+import * as norman from "norman"
+import { ResultSet, ResultSetDiff } from "norman"
+import { Observable } from "rxjs"
+
+export class get_user_by_login {
+    constructor(private login: string) {}
+}
+
+export class get_login_by_name {
+    constructor(private first: string) {}
+}
+
+export class NormanService {
+	observe(req: get_user_by_login): Observable<ResultSet<get_user_by_login_response>>;
+	observe(req: get_login_by_name): Observable<ResultSet<get_login_by_name_response>>;
+	observe(req: any): any { }
+	observeDiffs(req: get_user_by_login): Observable<ResultSetDiff<get_user_by_login_response>>;
+	observeDiffs(req: get_login_by_name): Observable<ResultSetDiff<get_login_by_name_response>>;
+	observeDiffs(req: any): any { }
+	query(req: get_user_by_login): Promise<ResultSet<get_user_by_login>>;
+	query(req: get_login_by_name): Promise<ResultSet<get_login_by_name>>;
+	query(req: any): any { }
+}
---
+++ example/querymanifest.ts
@@ -0,0 +1,42 @@
+import { QueryManifest, WhereRelationship } from "norman";
+
+export var manifest: QueryManifest = {
+    "tables": {
+        "user": {
+            "login": { "type": "varchar(256)" },
+             "passhash": { "type": "varchar(512)" },
+             "email" : { "type": "varchar(512" }
+        },
+        "contact": {
+            "user_login": { "type": "varchar(256)" },
+            "first": { "type": "varchar(128)" },
+            "last": { "type": "varchar(128)" }
+        }
+    },
+    "queries": [{
+        "name": "get_user_by_login",
+        "parameters": [{ "name": "login", "type": "user.login"}],
+        "effect": { 
+            "select": ["user.login", "user.email"],
+            "from": ["user"],
+            "where": {
+                "parameter": "login",
+                "relationship": WhereRelationship.Equals,
+                "dbvalue": "user.login"
+            }
+        },
+    },
+    {
+        "name": "get_login_by_name",
+        "parameters": [{ "name": "first", "type": "contact.first"}],
+        "effect": { 
+            "select": ["user.login", "user.email"],
+            "from": ["user"],
+            "where": {
+                "parameter": "login",
+                "relationship": WhereRelationship.Equals,
+                "dbvalue": "user.login"
+            }
+        },
+    }]
+}
